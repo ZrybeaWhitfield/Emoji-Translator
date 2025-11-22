f9072681010bbf6173e57562c560d81cff81b17a
ZrybeaWhitfield <zeathebeast@gmail.com>
2025-11-22 01:39:57 -0500

    Initial project setup, vite react FE, express BE

---
+++ server/index.js
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/health", (req, res) => {
+    res.json({ ok: true, message: "server is healthy" });
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
